c446a61322069cef6a8a6cab04672c632c975519
JenniferKiesler <111549041+JenniferKiesler@users.noreply.github.com>
2022-11-16 23:45:43 -0600

    adds database tables and connection info

---
+++ index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const cTable = require('console.table');
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
