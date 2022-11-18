0c55daf5f1600941fb2214fa4bdcadde1d7227cd
JenniferKiesler <111549041+JenniferKiesler@users.noreply.github.com>
2022-11-17 22:28:01 -0600

    adds menu prompt

--- index.js
+++ index.js
@@ -9,3 +9,34 @@ const connection = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 })
+
+const menuPrompt = async () => {
+    const answer = await inquirer.prompt([
+        {
+            type: "list",
+            name: "action",
+            message: "What do you want to do?",
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Exit']
+        }
+    ])
+
+    if (answer.action === 'View all departments') {
+        viewDepartments()
+    } else if (answer.action === 'View all roles') {
+        viewRoles()
+    } else if (answer.action === 'View all employees') {
+        viewEmployees()
+    } else if (answer.action === 'Add a department') {
+        addDepartment()
+    } else if (answer.action === 'Add a role') {
+        addRole()
+    } else if (answer.action === 'Add an employee') {
+        addEmployee()
+    } else if (answer.action === 'Update an employee role') {
+        updateEmployeeRole()
+    } else {
+        process.exit(0)
+    }
+}
+
+menuPrompt()
